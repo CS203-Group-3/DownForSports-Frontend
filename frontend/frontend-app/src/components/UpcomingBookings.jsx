25874d7a146cc378d9fae5e46cec14ba5bb29f28
JamondMak <jamond.mak.2022@scis.smu.edu.sg>
2023-11-02 16:41:46 +0800

    fix upcoming

--- frontend/frontend-app/src/components/UpcomingBookings.jsx
+++ frontend/frontend-app/src/components/UpcomingBookings.jsx
@@ -1,82 +1,112 @@
-import React, { useState } from 'react';
-import { Modal, Button, Card } from 'react-bootstrap';
+import React, { useState, useEffect, useRef } from 'react';
+import MyNavbar from './NavbarComp';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
+import { Card, Button, Modal } from 'react-bootstrap';
 
-function ConfirmAttendance() {
-  const [showModal, setShowModal] = useState(false);
-  const [bookingId, setBookingId] = useState(null);
-  const [bookings, setBookings] = useState([]);
+function UpcomingBookings() {
+  const [upcomingBookings, setUpcomingBookings] = useState([]);
+  const [showCancelModal, setShowCancelModal] = useState(false);
+  const [bookingToCancel, setBookingToCancel] = useState(null);
+  const userId = JSON.parse(localStorage.getItem('jwtResponse')).id;
 
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/api/bookings/viewupcomingbookings", {
+        params: {
+          userId: userId,
+        },
+      })
+      .then((response) => {
+        console.log("View response: ", response.data);
+        setUpcomingBookings(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching upcoming bookings:', error);
+      });
+  }, [userId]);
 
-  const handleOpenModal = (id) => {
-    setBookingId(id);
-    setShowModal(true);
+ 
+  const openCancelModal = (booking) => {
+    setBookingToCancel(booking);
+    setShowCancelModal(true);
   };
 
-  const handleCloseModal = () => {
-    setShowModal(false);
+  const closeCancelModal = () => {
+    setShowCancelModal(false);
+    setBookingToCancel(null);
   };
 
-  const handleConfirmAttendance = (attendanceStatus) => {
-    axios.post('http://localhost:8080/api/confirmbookingattendance', {
-      bookingId: bookingId,
-      attendanceStatus: attendanceStatus
-    })
-    .then((response) => {
-      console.log('Attendance confirmed successfully.');
-      handleCloseModal();
-    })
-    .catch((error) => {
-      console.error('Error confirming attendance:', error);
-    });
+  const confirmCancelBooking = () => {
+    if (bookingToCancel) {
+      console.log('Canceling booking with bookingId:', bookingToCancel.bookingId); // Log the bookingId
+      axios
+        .post("http://localhost:8080/api/bookings/cancelbooking", { bookingId: bookingToCancel.bookingId })
+        .then((response) => {
+          console.log("Booking canceled:", response.data);
+          // Handle success and display a confirmation message
+          // You can also refresh the list of upcoming bookings
+          closeCancelModal();
+        })
+        .catch((error) => {
+          console.error('Error canceling booking:', error);
+          // Handle the error and display an error message
+          closeCancelModal();
+        });
+    }
   };
 
   return (
     <div>
-      <h1>Confirm Attendance</h1>
-      <div className="container">
-        <div className="row">
-          {bookings.map((booking) => (
-            <div className="col-md-4 mb-3" key={booking.bookingId}>
-              <Card>
-                <Card.Body>
-                  <Card.Title>Facility: {booking.facility}</Card.Title>
-                  <Card.Text>
-                    <strong>Description:</strong> {booking.description}
-                    <br />
-                    <strong>Start Time:</strong> {booking.startTime}
-                    <br />
-                    <strong>End Time:</strong> {booking.endTime}
-                    <br />
-                    <strong>Date:</strong> {booking.date}
-                    <br />
-                    <strong>Location:</strong> {booking.location}
-                  </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                  <Button variant="primary" onClick={() => handleOpenModal(booking.bookingId)}>Confirm Attendance</Button>
-                </Card.Footer>
-              </Card>
-            </div>
-          ))}
-        </div>
-      </div>
+      <MyNavbar />
+      <h1>Upcoming Bookings</h1>
+      {upcomingBookings.length > 0 ? (
+               <div>
+               {upcomingBookings.map((booking, index) => (
+                         <Card key={index} className="mb-3">
+                         <Card.Body>
+                           <Card.Title>Facility: {booking.facility}</Card.Title>
+                           <Card.Text>
+                             <strong>Description:</strong> {booking.description}
+                             <br />
+                             <strong>Start Time:</strong> {booking.startTime}
+                             <br />
+                             <strong>End Time:</strong> {booking.endTime}
+                             <br />
+                             <strong>Date:</strong> {booking.date}
+                             <br />
+                             <strong>Location:</strong> {booking.location}
+                           </Card.Text>
+                         </Card.Body>
+                         <Card.Footer>
+                           <Button variant="danger" onClick={() => openCancelModal(booking)}>
+                             Cancel Booking
+                           </Button>
+                         </Card.Footer>
+                       </Card>
+                     ))}
+                             </div>
+      ) : (
+        <p>You have no upcoming bookings.</p>
+      )}
 
-      <Modal show={showModal} onHide={handleCloseModal}>
+      <Modal show={showCancelModal} onHide={closeCancelModal}>
         <Modal.Header closeButton>
-          <Modal.Title>Confirm Attendance</Modal.Title>
+          <Modal.Title>Confirm Cancellation</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <p>Did you attend this booking?</p>
-          <Button onClick={() => handleConfirmAttendance(1)}>Present</Button>
-          <Button onClick={() => handleConfirmAttendance(-1)}>No Show</Button>
+          Are you sure you want to cancel this booking?
         </Modal.Body>
         <Modal.Footer>
-          <Button onClick={handleCloseModal}>Cancel</Button>
+          <Button variant="secondary" onClick={closeCancelModal}>
+            No, Go Back
+          </Button>
+          <Button variant="danger" onClick={confirmCancelBooking}>
+            Yes, Cancel Booking
+          </Button>
         </Modal.Footer>
       </Modal>
     </div>
   );
 }
-
-export default ConfirmAttendance;
+export default UpcomingBookings;
